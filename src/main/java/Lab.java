b0985614b447c000f8f4b7ff153f936589f11ff3
Jericho Nasser <jerichonasser@yahoo.com>
2023-10-15 15:51:17 +0000

    JN

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,4 +1,5 @@
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -19,7 +20,8 @@ public class Lab {
      * @return a new Set<Integer> object.
      */
     public Set<Integer> createSet(){
-        return null;
+        Set<Integer> set = new HashSet<Integer>();
+        return set;
     }
 
     /**
@@ -28,7 +30,7 @@ public class Lab {
      * @return the size of set. (number of values)
      */
     public int getSize(Set<Integer> set){
-        return 0;
+        return set.size();
     }
 
     /**
@@ -38,7 +40,11 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addUniqueItem(Set<Integer> set, int item){
-
+        if (!set.contains(item)) {
+            set.add(item);
+        } else {
+            System.out.println("Item is duplicate");
+        }
     }
 
     /**
@@ -48,6 +54,10 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeItem(Set<Integer> set, int item){
-
+        if(set.contains(item)){
+            set.remove(item);
+        }else{
+            System.out.println("item not in set");
+        }
     }
 }
